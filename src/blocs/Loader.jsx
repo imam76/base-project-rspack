3a111ff9d015a5409283baa560cfef6774689627
imam76 <asepimam335@gmail.com>
2025-05-01 08:11:37 +0700

    add spinner loader

---
+++ src/blocs/Loader.jsx
@@ -0,0 +1,17 @@
+import { Spin } from 'antd';
+
+const Loader = () => {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100%',
+      width: '100%'
+    }}>
+      <Spin size="large" />
+    </div>
+  );
+};
+
+export default Loader;
--- src/utils/reactLazyWithSuspense.jsx
+++ src/utils/reactLazyWithSuspense.jsx
@@ -1,10 +1,11 @@
+import Loader from '@/blocs/Loader';
 import { Suspense, lazy } from 'react';
 
 const ReactLazyWithSuspense = (importFunc) => {
   const LazyComponent = lazy(importFunc);
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<Loader />}>
       <LazyComponent />
     </Suspense>
   );

f4e7ba294c8830cc9ccf747ee292d2d4a288d031
imam76 <asepimam335@gmail.com>
2025-05-05 07:01:26 +0700

    add: integrate Vercel Speed Insights for performance monitoring

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import { ConfigProvider } from 'antd';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import('@ant-design/v5-patch-for-react-19');
-
 import App from './App';
 import { theme } from './styles/theme';
 import('./index.css');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <SpeedInsights />
     <ConfigProvider theme={theme}>
       <App />
     </ConfigProvider>

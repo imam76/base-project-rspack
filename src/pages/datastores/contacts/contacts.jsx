dbd59ced4e40ad18a54532693f63e2409937ffd1
imam76 <asepimam335@gmail.com>
2025-07-09 10:41:11 +0700

    add phone -> contact

--- src/pages/datastores/contacts/contacts.jsx
+++ src/pages/datastores/contacts/contacts.jsx
@@ -20,7 +20,6 @@ import { useEffect, useState } from 'react';
 import { Outlet, useNavigate } from 'react-router';
 
 import ContextMenuOption from '@/blocs/ContextMenuOption';
-import { Button as LucideButton } from '@/components/LucideButton';
 import { useDataQuery } from '@/utils/hooks/useDataQuery';
 import { useDebouncedSearchParams } from '@/utils/hooks/useDebouncedSearchParams';
 import useExportCSV from '@/utils/hooks/useExportCSV';
